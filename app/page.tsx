f742e1c8e36d177f3923c31371de61d2f1c81cb6
joaoleandro <new.joaoleandro@gmail.com>
2024-06-19 21:17:22 -0300

    feat: homepage, add libry daisy ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
-    <div>
-      DASHBOARD
-    </div>
+    <section className="w-screen h-screen flex flex-col items-center justify-center gap-y-3">
+      <button className="btn btn-success w-72 text-white">login</button>
+      <button className="btn btn-success w-72 text-white">Registre-se</button>
+    </section>
   );
 }
